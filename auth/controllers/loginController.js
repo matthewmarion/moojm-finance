6bc9fece2d59f8c2306fad01024638236f2d750b
Matthew <mm185568@ncr.com>
2020-07-28 02:23:06 -0400

    auth service work

--- auth/controllers/loginController.js
+++ auth/controllers/loginController.js
@@ -1,7 +1,17 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../firebaseServiceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_URL
+});
+
+const db = admin.firestore();
+
 const loginController = {};
 
 loginController.getAccounts = async (req, res) => {
-    res.status(200).send("Data");
+    res.status(200).send("Data")
 };
 
 module.exports = loginController;
